3be1b1497f0b992f95935c5394e06e257c3ac245
hissatumanabinin <89464989b.c@gmail.com>
2020-01-31 12:53:24 +0000

    realend

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -82,6 +82,9 @@ class TasksController extends Controller
      */
     public function update(Request $request, $id)
     {
+        $this->validate($request,[
+            'status'=>'required|max:10',
+            ]);
         $task=Task::find($id);
         $task->content=$request->content;
         $task->status=$request->status;
